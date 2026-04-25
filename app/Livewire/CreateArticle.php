083bca3d729310cf24263a7c1ddc31b63daf0134
Simone Mandorlini <djbaffo85@gnail.com>
2026-04-25 15:17:25 +0200

    aggiunta validazione campi form create-article

--- app/Livewire/CreateArticle.php
+++ app/Livewire/CreateArticle.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Article;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -10,24 +11,35 @@ class CreateArticle extends Component
 {
     use WithFileUploads;
 
+    #[Validate('required', message: 'Il titolo è obbligatorio')]
+    #[Validate('min:5', message: 'Deve contenere almeno 5 caratteri')]
     public $title;
+
+    #[Validate('required', message: 'La descrizione è obbligatoria')]
+    #[Validate('min:20', message: 'Deve contenere almeno 20 caratteri')]
     public $description;
+
+    #[Validate('required', message: 'Il corpo è obbligatorio')]
+    #[Validate('min:50', message: 'Deve contenere almeno 50 caratteri')]
     public $body;
+
     public $img;
-    
 
 
-    public function store(){
-        
+
+    public function store()
+    {
+
+        $this->validate();
+
         $article = [
             'title' => $this->title,
             'description' => $this->description,
             'body' => $this->body,
-            ];
+        ];
 
-        if($this->img){
+        if ($this->img) {
             $article['img'] = $this->img->store('media/img', 'public');
-            
         }
 
         Article::create($article);
--- resources/views/livewire/create-article.blade.php
+++ resources/views/livewire/create-article.blade.php
@@ -3,15 +3,20 @@
     <form wire:submit="store" class="shadow p-4 rounded bg-warning bg-opacity-25">
         <div class="mb-3">
             <label for="title" class="form-label">Titolo Articolo</label>
-            <input wire:model="title" type="text" class="form-control" id="title">
+            <input wire:model.live.blur="title" type="text" class="form-control" id="title">
+            @error('title')<div class="bg-danger-subtle mt-1 p-1 rounded "> {{ $message }}</div>@enderror
+
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Breve Descrizione</label>
-            <input wire:model="description" type="text" class="form-control" id="description">
+            <input wire:model.live.blur="description" type="text" class="form-control" id="description">
+            @error('description')<div class="bg-danger-subtle mt-1 p-1 rounded "> {{ $message }}</div>@enderror
+
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">Corpo dell'articolo</label>
-            <textarea wire:model="body" id="body" class="form-control" rows="8"></textarea>
+            <textarea wire:model.live.blur="body" id="body" class="form-control" rows="8"></textarea>
+            @error('body')<div class="bg-danger-subtle mt-1 p-1 rounded "> {{ $message }}</div>@enderror
         </div>
 
         <div class="mb-3">
