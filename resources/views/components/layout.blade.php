daab6765c3757fc27deefc5bc421255a3ac08672
Simone Mandorlini <djbaffo85@gnail.com>
2026-04-25 12:58:15 +0200

    creato modello articolo e migrazioni

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/css/app.css', 'resources/css/app.js'])
+
+    <title>{{$title}}</title>
+</head>
+
+<body>
+
+    <x-navbar />
+
+    <x-masthead title="{{$title}}"/>
+
+
+
+</body>
+
+</html>
